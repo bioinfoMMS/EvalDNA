7032df889dc882a84aa2267e2e261d827ee9a67e
Maddy MacDonald <maddym_21@hotmail.com>
2021-09-09 16:44:40 -0400

    Update example_run_evaldna_docker.sh

--- example_run_evaldna_docker.sh
+++ example_run_evaldna_docker.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+DIR = "" #Set DIR variable to the directory where you cloned the EvalDNA repository
 cd EvalDNA_v1.1
 
-docker build -t evaldna_test . 
+docker build -t evaldna . 
 
-docker run -t -u $(id -u):$(id -g) -v ./EvalDNA/EvalDNAv1.1/results:/usr/src/app/results -v ./EvalDNA/EvalDNA_v1.1/code:/usr/src/app/code -v ./EvalDNA/EvalDNA_v1.1/data:/usr/src/app/data --name edtest -t --cpus="16" evaldna_test
+docker run -t -u $(id -u):$(id -g) -v $DIR/EvalDNA/EvalDNAv1.1/results:/usr/src/app/results -v $DIR/EvalDNA/EvalDNA_v1.1/code:/usr/src/app/code -v $DIR/EvalDNA/EvalDNA_v1.1/data:/usr/src/app/data --name edtest -t --cpus="16" evaldna
 
 docker container rm --force edtest
 
